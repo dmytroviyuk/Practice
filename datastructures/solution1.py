59d57bb4065bc417aea48b42b6cfbbe1b3743d9a
Dmytro Viyuk <dmytro.viyuk@gmail.com>
2021-05-26 17:02:56 +0300

    Added new code: Updated solution.py and added code to solution1.py

--- datastructures/solution1.py
+++ datastructures/solution1.py
@@ -6,26 +6,60 @@ import random
 import re
 import sys
 
-# Complete the reverseArray function below.
-def reverseArray(a):
-    n = 3
-    b13 = [1,4,3,2]
-    b14 = [1,4,3,2]
-    for i in range (0, n+1):
-        b14[i] = b13[n]
-        n = n-1
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
 
-    return b14
+def hourglassSum(arr):
+    sparearray = []
+    locationx = 1
+    FIDIM = 0
+    locationy = 1
+    goodboypoints = 0
+    chosenone = 0
+    for i in range(15):
+        if  FIDIM == 4 :
+            locationx = 1
+            locationy=locationy+1;
+            FIDIM = 0
+        locationxplus=locationx+1
+        locationxminus=locationx-1
+
+        locationyplus=locationy+1
+        locationyminus=locationy-1
+        FIDIM = FIDIM+1
+        
+        sum=arr[locationy][locationx]+arr[locationyplus][locationx]+arr[locationyminus][locationx]+arr[locationyminus][locationxminus]+arr[locationyminus][locationxplus]+arr[locationyplus][locationxplus]+arr[locationyplus][locationxminus]
+        sparearray.append(sum)
+        locationx=locationx+1
+        
+    print(sparearray)
+    for i in range(15):
+        goodboypoints = 0
+        for i2 in range(15):
+            if sparearray[i] >= sparearray[i2]:
+                goodboypoints=goodboypoints+1
+            if goodboypoints==15:
+                print("triggered")
+                chosenone = sparearray[i]
+        print("Divider:"+str(chosenone))
+           
+    return(chosenone)
+        
+        
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    arr_count = int(input())
+    arr = []
 
-    arr = list(map(int, input().rstrip().split()))
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
 
-    res = reverseArray(arr)
+    result = hourglassSum(arr)
 
-    fptr.write(' '.join(map(str, res)))
-    fptr.write('\n')
+    fptr.write(str(result) + '\n')
 
     fptr.close()
